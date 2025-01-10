c6bab18ba8e26c38ae9ab1267ba35adf81854218
Marlon Ernst <e263206@edu.sbl.ch>
2025-01-10 08:33:29 +0000

    Level-00 cursor Bewegungen

--- src/game/scenes/levels/level-00.js
+++ src/game/scenes/levels/level-00.js
@@ -46,4 +46,24 @@ export default class Level00 extends Phaser.Scene {
     // Stelle die Kamera so ein, dass sie dem Spieler folgt.
     this.cameras.main.startFollow(this.player)
   }
+
+  update() {
+    // Hole die teile left, right, up und down aus dem Cursor-Objekt
+    const { left, right, up, down } = this.cursor
+
+    // Wenn links gedrückt wird...
+    if (left.isDown) {
+      // Links wurde gedrückt, verschiebe den Spieler einen Pixel nach rechts.
+      this.player.x -= 1
+    }
+    if (right.isDown) {
+      this.player.x += 1
+    }
+    if (up.isDown) {
+      this.player.y -= 1
+    }
+    if (down.isDown) {
+      this.player.y += 1
+    }
+  }
 }
